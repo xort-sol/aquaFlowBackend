b825a5bcf9521d1bd7a1a11d2c13dad84b1272ff
Umer Ghafoor <umerghaforr@gmail.com>
2025-10-08 00:47:50 +0500

    fix(db): ensure server selection timeout is set for MongoDB connection

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000 // 30 seconds
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
